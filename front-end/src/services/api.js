1115245b75f2d1d17e4927611769a616f037a262
VedangPaithankar <98870775+VedangPaithankar@users.noreply.github.com>
2024-11-06 13:39:51 +0530

    Update api.js

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://nlp-news-api.onrender.com/api';
 
 // Summarize Article
 export const summarizeArticle = (url) => {
